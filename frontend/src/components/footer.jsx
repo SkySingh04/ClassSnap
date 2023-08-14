46a8ba36fba947a1648c3f18e84c7e820a57c8ba
Akash <akashsingh2210670@gmail.com>
2023-08-14 22:55:34 +0530

    Finished The Main page

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-primary text-white text-lg p-4 text-center">
+      © 2023 ClassSnap. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/homePage.js
+++ frontend/src/homePage.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Book, Edit, Sync } from '@mui/icons-material';
-import backgroundImage from './onlineclass3.png'
+
+
+
+import AkashSingh from './images/AkashSingh.jpeg';
+import ShivanshKaran from "./images/ShivanshKaran.jpeg"
+import Gautam from "./images/GautamShorewala.png"
+import Shashwat from "./images/Shashwat.png"
+
+import backgroundImage from './images/onlineclass.png'
+import Problem from "./images/Problem.png"
+import Solution from "./images/Solution.png"
+import  Github  from './images/Github.png';
+
+
+import './index.css';
+
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+
 function HomePage() {
 
     const features = [
@@ -22,62 +39,170 @@ function HomePage() {
         },
       ];
 
+    const teamMembers = [
+      {
+        name: 'Akash Singh',
+        github: 'https://github.com/Akash-Singh04/ClassSnap',
+        profilePic: AkashSingh,
+      },
+      {
+        name: 'Gautam Shorewala',
+        github: 'https://github.com/GautamCoder4019k',
+        profilePic: Gautam,
+      },
+      {
+        name: 'Shashwat Kumar',
+        github: 'https://github.com/shashwat6204',
+        profilePic: Shashwat,
+      },
+      {
+        name: 'Shivansh Karan',
+        github: 'https://github.com/SpaceTesla',
+        profilePic: ShivanshKaran,
+      },
+    ];
 
+    const usageInstructions = [
+      "Write instructions after project finish",
+      "Sign up for an account on ClassSnap.",
+      "Link your online learning platforms for synchronization.",
+      "Access your class presentations through ClassSnap.",
+      "Browse the generated notes and summaries for each presentation.",
+    ];
+
+
+    const problemStatement = "Traditional student information management systems often struggle to keep up with the increasing demand for streamlined administrative processes and personalized learning experiences. In parallel, online learning platforms have become prevalent, offering extensive course materials and digital notes. However, students often face challenges in efficiently processing and assimilating this much of information, leading to reduced learning effectiveness and academic performance.";
+
+    const proposedSolution = "ClassSnap is an advanced web application designed to enhance student's learning experience by providing personalized access to academic details and automatically generating comprehensive notes through intelligent presentation summarization. Leveraging cutting-edge technologies, the platform employs Machine Learning algorithms to perform intelligent summarization of presentations from online class sessions. Through the use of Selenium Web Driver, ClassSnap automatically extracts relevant information from the meeting presentations and processes it with the Machine Learning algorithm to generate organized and concise notes, reducing manual effort for students. The application seamlessly integrates with popular online learning platforms, ensuring effortless synchronization of academic data.";
+
+    
+    
   return (
     <div className="bg-background text-primary min-h-screen flex flex-col relative">
 
-      {/* Navbar */}
-      <nav className="bg-secondary p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white font-bold text-xl">ClassSnap</div>
-          <div>
-            <Link to="/login" className="text-white mx-2">Login</Link>
-            <Link to="/signup" className="text-white mx-2">Signup</Link>
-          </div>
-        </div>
-      </nav>
-
+      <Navbar />
       {/* Hero Section */}
-      <section
-        className="flex-1 container mx-auto py-32 text-center relative"
+      <section 
+        className="flex-1 h-full container mx-auto py-32 text-center relative"
         style={{
           backgroundImage: `url(${backgroundImage})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
       >
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center">
+        <div className="absolute  inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold text-white mb-4">Welcome to ClassSnap</h1>
           <p className="text-lg text-white">
-            Enhance your learning experience with personalized access to academic details and automated note generation.
+            Enhance your learning experience today!
           </p>
         </div>
       </section>
+        
+        {/* Problem and Solution Section */}
+      <section  className="bg-primary py-16 px-8">
+        <div className="container mx-auto text-center">
+          <div className="grid gap-8 md:grid-cols-2">
+            {/* Left Side: Problem Statement with Image */}
+            <div id="problem" className="flex flex-col justify-center items-start">
+              <h1 className="text-4xl font-bold mb-8 text-white">The Problem?</h1>
+              <p className="text-lg text-left text-white">{problemStatement}</p>
+            </div>
+            <div className="md:flex md:justify-end md:items-center">
+              <img src={Problem} alt="Solution" className="max-w-base md:w-3/4" />
+            </div>
+            
+            
+            {/* Right Side: Proposed Solution with Image */}
+            
+            <div id="solution" className="md:flex md:items-center">
+              <img src={Solution} alt="Solution" className="max-w-base md:w-3/4" />
+            </div>
+
+            <div className="flex flex-col justify-center items-end">
+            
+              <h2 className="text-4xl font-bold mb-8 text-right text-white">The Solution!</h2>
+              <p className="text-lg text-right text-white">{proposedSolution}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      
 
       {/* Features Section */}
-      <section className="bg-secondary py-16 px-6">
+      <section id="features" className="bg-secondary py-16 px-6">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-8">Features</h2>
           <div className="grid gap-6 md:grid-cols-3">
             {features.map((feature, index) => (
               <div
                 key={index}
-                className="p-6 bg-white shadow-lg rounded-lg hover:scale-105 transition-transform"
+                className="p-6 bg-primary shadow-lg rounded-lg hover:scale-105 transition-transform"
               >
                 <div className="text-3xl mb-4 text-accent">{feature.icon}</div>
-                <h3 className="text-xl font-semibold mb-2">{feature.title}</h3>
-                <p className="text-gray-700">{feature.description}</p>
+                <h3 className="text-xl text-white font-semibold mb-2">{feature.title}</h3>
+                <p className="text-white">{feature.description}</p>
               </div>
             ))}
           </div>
         </div>
       </section>
+
+      {/* Usage Instructions Section */}
+      <section id="instructions" className="bg-primary py-16 px-8">
+        <div className="container mx-auto text-center">
+          <div className="grid gap-8 md:grid-cols-2">
+            {/* Left Side: Usage Instructions */}
+            <div className="md:flex md:flex-col md:justify-center">
+              <h2 className="text-4xl text-white font-bold mb-4 text-left">How to Use?</h2>
+              <ul className="list-disc list-inside text-white text-left">
+                {usageInstructions.map((instruction, index) => (
+                  <li key={index} className="mb-2">
+                    {instruction}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex items-center justify-center">
+            <a href="https://github.com/Akash-Singh04/ClassSnap">
+            <img src={Github} alt="Arrow" className=" h-1/4 " />/
+            <h1 className="text-xl font-semibold text-accent">View Source Code</h1>
+            </a>
+          </div>
+            
+          </div>
+        </div>
+      </section>
+      {/* Meet the Team Section */}
+      <section id="team" className="bg-secondary py-16">
+        <div className="container mx-auto text-center">
+          <h2 className="text-3xl font-bold mb-8">Meet the Team</h2>
+          <div className="grid gap-4 md:grid-cols-4">
+            {teamMembers.map((member, index) => (
+              <div key={index} className="p-4 bg-primary shadow-lg rounded-lg">
+                <img
+                  src={member.profilePic}
+                  alt={`${member.name} Profile`}
+                  className="w-20 h-20 mx-auto mb-2 rounded-full"
+                />
+                <h3 className="text-lg text-accent font-semibold mb-2">{member.name}</h3>
+                <a
+                  href={member.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-accent hover:underline"
+                >
+                  GitHub Profile
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <Footer />
       
-      {/* Footer */}
-      <footer className="bg-secondary py-4 text-center">
-        <p>&copy; 2023 ClassSnap. All rights reserved.</p>
-      </footer>
+      
     </div>
+      
   );
 }
 
