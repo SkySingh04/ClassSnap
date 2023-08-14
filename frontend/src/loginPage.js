974111e6fd5a45188254c115df545187a0a7a94e
Gautam Shorewala <123801344+GautamCoder4019k@users.noreply.github.com>
2023-08-14 13:43:18 +0530

    Bhai galti se secret exposed tha

--- frontend/src/loginPage.js
+++ frontend/src/loginPage.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Perform login logic here
-    // For demonstration, let's assume successful login
-
-    navigate(`/user/${username}`);
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/login', { username, password });
+      console.log(response.data);
+      navigate(`/user/${username}`);
+       // Response from the backend
+    } catch (error) {
+      console.error('Login error:', error);
+      window.alert( error.response.data.error);
+    }
   };
 
   return (
     <div>
       <h2>Login</h2>
       <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={handleLogin}>Login</button>
     </div>
   );
