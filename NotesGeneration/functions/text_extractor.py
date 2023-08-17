94d2d3bceff97b4316e6ce8867eab240b5f47cf6
Shivansh Karan <96942282+SpaceTesla@users.noreply.github.com>
2023-08-17 23:14:22 +0530

    🚀Updating text extraction Now er can extract text from both audio and video 🔥

--- NotesGeneration/functions/text_extractor.py
+++ NotesGeneration/functions/text_extractor.py
@@ -1,14 +1,55 @@
+# call extract_text(video_path) function to extract text from video and save it to a file.
 import cv2
 import pytesseract
 
+from moviepy.editor import VideoFileClip
+import assemblyai as aai
+import os
+from dotenv import load_dotenv
 
+
+# Getting API token from .env file
+load_dotenv()
+MY_ENV_VAR = os.getenv('AAI_API_KEY')
+aai.settings.api_key = MY_ENV_VAR
+
+
+def audio_to_text(video_path: str) -> None:
+    """Convwer audio to text using AssemblyAI API and save it to a file.
+
+    Args:
+        video (str): Path to video file.
+    """
+        
+    # Load the video file
+    video = VideoFileClip(video_path)
+    # Extract the audio from the video
+    audio = video.audio
+    # Save the audio to a file
+    audio.write_audiofile("audio.wav")
+
+
+    # Create a transcriber object.
+    transcriber = aai.Transcriber()
+
+
+    transcript = transcriber.transcribe("audio.wav")
+
+
+    # transcript = transcriber.transcribe("https://storage.googleapis.com/aai-web-samples/espn-bears.m4a")
+
+    with open("audio_text.txt", "w") as f:
+        f.write(transcript.text)
+
+
+# --- From Video File ---
 def write_to_file(text):
-    with open("extracted_text.txt", "a") as f:
+    with open("video_text.txt", "a") as f:
         f.write(text)
         f.write("\n")
 
-# Import this function
-def extract_text(video_path: str) -> None:
+
+def video_to_text(video_path: str) -> None:
     """Use RAW STRING `(r"")` for video_path (i.e. `r"folder\\file"`)
 
     Args:
@@ -20,7 +61,7 @@ def extract_text(video_path: str) -> None:
 
     # Open the video file
     cap = cv2.VideoCapture(fr"{video_path}")
-    open("extracted_text.txt", "w").close()
+    open("video_text.txt", "w").close()
 
 
     # Read the first frame
@@ -88,3 +129,24 @@ def extract_text(video_path: str) -> None:
     # Release the video capture object and close all windows
     cap.release()
     cv2.destroyAllWindows()
+
+# Import this function
+def extract_text(video_path: str) -> None:
+    """Extract text from video and save it to a file.
+
+    Args:
+        video_path (str): Path to video file.
+    """
+    audio_to_text(video_path)
+    video_to_text(video_path)
+    
+    # Creating final text file
+    with open("video_text.txt", "r") as f:
+        video_text = f.read()
+    with open("audio_text.txt", "r") as f:
+        audio_text = f.read()
+    with open("final_text.txt", "w") as f:
+        f.write(video_text)
+        f.write("\n")
+        f.write(audio_text)
+
