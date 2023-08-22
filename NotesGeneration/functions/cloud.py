e3ec99ce94c23be8fa686da91dbf929c10720610
Shashwat <shashwatcse6204@gmail.com>
2023-08-22 22:00:04 +0530

    pdf to cloudinary

---
+++ NotesGeneration/functions/cloud.py
@@ -0,0 +1,18 @@
+import cloudinary
+import cloudinary.uploader
+
+# Configure Cloudinary with your credentials
+cloudinary.config(
+    cloud_name='dptrkbl3o',
+    api_key='613799345313771',
+    api_secret='_u5ycqFktrQ7gK61fdfHjJDWjWU'
+)
+
+# Path to the PDF file you want to upload
+pdf_path = './NotesGeneration/assets/rearranged.pdf'
+
+# Upload the PDF
+result = cloudinary.uploader.upload(pdf_path, resource_type='auto')
+
+# Print the result
+print(result)
